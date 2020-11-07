7272403d2163fd2110453640d6fe19cbc3a03635
João Pedro Maciel <joomaciiel2@outlook.com>
2020-11-07 17:29:44 -0300

    spotify-web-api-js connection to user

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import Login from './Components/Login/Login';
+import Player from './Components/Player/Player';
 import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from 'spotify-web-api-js';
+
+// Communication with spotify
+const spotify = new SpotifyWebApi();
 
 function App() {
 
@@ -17,6 +22,12 @@ function App() {
 
     if (_token) {
       setToken(_token);
+      spotify.setAccessToken(_token);
+
+      // get user account
+      spotify.getMe().then(user => { 
+        console.log(user);
+      }); 
     }
 
   }, []);
@@ -26,7 +37,7 @@ function App() {
       
       { // if have token, return the PlayerComponent, else return LoginComponent
         token ?
-          {/*<Player />*/}
+          <Player />
          : (
           <Login />
         )
---
+++ src/Components/Player/Player.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Player() {
+  return (
+    <div className="player">
+      <h1>Player Component</h1>
+    </div>
+  )
+}
+
+export default Player
